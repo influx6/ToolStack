da563462a554e98fd865b59b1f9630da86bc3dbf
Alexander Ewetumo <trinoxf@gmail.com>
2012-10-25 18:35:00 +0100

    change name to appstack

---
+++ appstack.js
@@ -0,0 +1,94 @@
+(function(r){
+      //all functions/methods with an '_' starting their names are hidden and are
+      //not to be called directly unless you need to test those specific
+      //operations directly,please use such a convention for a better modularizing
+      //of code
+
+      var root = r;
+
+      var previousAppStack = root.AppStack;
+
+      var AppStack;
+      
+
+      if(root.exports !== undefined){
+         root.exports = AppStack;
+      }else{
+         AppStack = root.AppStack = function(){};
+      }
+
+      AppStack.ObjectClassName = "AppStack";
+
+      AppStack.noConflict = function(){
+        root.AppStack = previousAppStack;
+        return this; 
+      };
+
+      //the current in use version of Class
+      AppStack.version = "0.3.2";
+
+      //config option to decide if Class that are created are to have the
+      //Class.Events available as part of their prototype or not
+     // AppStack.useEvents = true;
+
+      //this class level method handles classic-type inheritance,ensure to use it before
+      //assigning any properties to the child prototype,incase of using the Class class without
+      //using its create method
+      AppStack.inherit = function(child,parent){
+         
+         function empty(){};
+         empty.prototype = parent.prototype;
+         
+         child.prototype = new empty();
+         
+         child.prototype.constructor = child;
+         child.parent = parent.prototype;
+         
+         parent.prototype.constructor = parent;
+
+      };
+
+      //please note that AppStack.mixin and AppStack.SU.extends careless about wether
+      //the arguments passed are objects or pure prototypes,they require you to
+      //specify,if its from a prototype to a prototype or they will walk with any
+      //object you give them as they are without looking or trying to add to this
+      //objects/functions prototype,its done this way to allow more flexibility 
+      //when desire to not just mix between prototypes but also between objects or
+      //objects constructors
+      AppStack.mixin = function(from,to){
+         for(var e in from){
+            if(e in to) return;
+            to[e] = from[e];
+         }
+      };
+
+       
+      AppStack.ns = function(space,fn){
+         var self = this,
+            space = space.split('.'),
+            splen = space.length,
+            index = 0,
+            current = null,
+            adder = function(obj,space,fn){ 
+               if(!obj[space]) obj[space] = fn ? fn : {};
+               return obj[space];
+            };
+
+
+         while(true){
+            if(index >= splen){
+               if(fn) adder(self,space[index - 1],fn)
+               self = this; break;
+            };
+            //we get the item,we add and move into lower levels
+            current = space[index];
+            self = adder(self,current);
+            index++;
+         };
+
+         return self;
+      };
+
+
+  
+})(this);
---
+++ extensions/appstack.class.js
@@ -0,0 +1,68 @@
+var Class = (function(EM){
+	return EM.create("Class",function(){
+
+		return {
+			name: "AppStack.Class",
+			version: "0.0.2",
+			description: "basic class structure for your js apps",
+	        licenses:[ { type: "mit", url: "http://mths.be/mit" }],
+	        author: "Alexander Adeniyin Ewetumo",
+
+      		create : function(classname,ability,parent){
+         
+		         var Class = function(){
+		            if(!(this instanceof arguments.callee)){
+		               return new arguments.callee;
+		            }
+		         };
+		         
+		         if(parent){ AppStack.inherit(Class,parent)};
+		         
+		         if(ability){
+		            if(!ability.instance && !ability.static){ 
+		               AppStack.SU.extends(Class.prototype, ability);
+		            }
+		            if(ability.instance){ 
+		               AppStack.SU.extends(Class.prototype, ability.instance);
+		            }
+		            if(ability.static){ 
+		               AppStack.SU.extends(Class,ability.static);
+		            }
+		         }
+		         
+		         //shortcut to all Class objects prototype;
+		         Class.fn = Class.prototype;
+		         //sets the className for both instance and Object level scope
+		         Class.ObjectClassName = Class.fn.ObjectClassName = classname;
+		         
+		         Class.fn.setup = function(){
+		            if(Class.parent){
+		               Class.parent.constructor.apply(this,arguments);
+		               this.super = Class.parent;
+		               if(this.super.init && typeof this.super.init === 'function'){
+		                  this.super.init.apply(this,arguments);
+		               }		
+		            }
+		           
+		            if(this.init && typeof this.init === 'function'){
+		               this.init.apply(this,arguments);
+		            }
+		            
+		            return this;
+		         };
+		         
+		         Class.extend = AppStack.extend;
+		         Class.mixin = AppStack.mixin;
+
+		         return Class;
+		      },
+
+		      //allows a direct extension of the object from its parent directly
+		      extend : function(name,ability){
+		         return AppStack.create(name,ability,this);
+		      }
+
+		};
+
+	});
+});
